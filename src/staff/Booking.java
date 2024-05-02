d7735cb50e2c93990966901b5d116aa7ad76b46a
Saisree1998 <165273741+Saisree1998@users.noreply.github.com>
2024-05-02 15:47:18 +0100

    Update Booking.java

--- src/staff/Booking.java
+++ src/staff/Booking.java
@@ -11,6 +11,16 @@ public class Booking {
     private final SimpleStringProperty bookingTime;
     private final SimpleStringProperty status;
 
+    /**
+     * Constructs a new Booking object.
+     *
+     * @param name         The name of the customer making the booking.
+     * @param tableSeat    The number of table seats reserved for the booking.
+     * @param bookingDate  The date of the booking.
+     * @param bookingTime  The time of the booking.
+     * @param status       The status of the booking.
+     */
+
     public Booking(String name, int tableSeat, String bookingDate, String bookingTime, String status) {
         this.name = new SimpleStringProperty(name);
         this.tableSeat = new SimpleIntegerProperty(tableSeat);
@@ -18,7 +28,11 @@ public class Booking {
         this.bookingTime = new SimpleStringProperty(bookingTime);
         this.status = new SimpleStringProperty(status);
     }
-
+    /**
+     * Gets the name of the customer making the booking.
+     *
+     * @return The name of the customer.
+     */
     public String getName() {
         return name.get();
     }
